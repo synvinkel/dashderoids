c17b9aeb299ec78daf4d6568e841e522fed9fca6
Johnnie Hård <johnnie@synvinkel.org>
2019-11-06 20:09:34 +0100

    color constants

---
+++ Cols.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var green : Color = Color("#d4feea")
+var blue : Color = Color("#71bcd3")
+var red : Color = Color("#df3224")
+var purple : Color = Color("#c89acb")
+var pink : Color = Color("#e5729b")
+
+func _ready():
+    pass
