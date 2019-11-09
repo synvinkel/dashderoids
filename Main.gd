34e05ca8f57f710f0619ce079f4aba2ee06c7595
Johnnie Hård <johnnie@synvinkel.org>
2019-11-09 09:15:19 +0100

    place polygon in middle of screen

--- Main.gd
+++ Main.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-    pass
+    var v = get_viewport().size
+    $Polygon.position = Vector2(v.x / 2, v.y /2)
     
 func _process(delta):
     if Input.is_action_just_pressed("ui_cancel"):
