ed13c2bff14a459333dcb371554dc970fc4ed971
Johnnie Hård <johnnie@synvinkel.org>
2019-11-12 22:16:00 +0100

    Pause screen working

--- Main.gd
+++ Main.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
+var paused : bool = false setget pause_game
+
 func _ready():
     var v = get_viewport().size
-    $Polygon.position = Vector2(v.x / 2, v.y /2)
+    $Game/Polygon.position = Vector2(v.x / 2, v.y /2)
     
 func _process(delta):
     if Input.is_action_just_pressed("ui_cancel"):
-        get_tree().reload_current_scene()
+#        get_tree().reload_current_scene()
+        if paused:
+            self.paused = false
+        else:
+            self.paused = true
         
 func quit_game():
     get_tree().quit()
     
 func start_game():
     $StartScreen.visible = false
-    $Skepp.playing = true
+    $Game/Skepp.playing = true
+    self.paused = false
+    
+func pause_game(_paused):
+    paused = _paused
+    get_tree().paused = paused
+    $StartScreen.visible = paused
+    
