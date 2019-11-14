7946443ff30495c514b1abddb2472c07c50a24c0
Johnnie Hård <johnnie@synvinkel.org>
2019-11-14 21:45:42 +0100

    pause

--- Main.gd
+++ Main.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
+var game_started : bool = false
+
 func _ready():
     var v = get_viewport().size
-    $Polygon.position = Vector2(v.x / 2, v.y /2)
+    $Game/Polygon.position = Vector2(v.x / 2, v.y /2)
+    
+func pause() -> void:
+    if get_tree().paused:
+        get_tree().paused = false
+        $StartScreen.visible = false
+    else:
+        get_tree().paused = true
+        $StartScreen.visible = true
     
 func _process(delta):
     if Input.is_action_just_pressed("ui_cancel"):
-        get_tree().reload_current_scene()
+        pause()    
         
 func quit_game():
     get_tree().quit()
     
 func start_game():
-    $StartScreen.visible = false
-    $Skepp.playing = true
+    if game_started:
+        pause()
+    else:
+        $StartScreen.visible = false        
+        $Game/Skepp.playing = true
