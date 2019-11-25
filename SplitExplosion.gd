5f7ef941be62fef818cb9419c8f1e001942af341
Johnnie Hård <johnnie@synvinkel.org>
2019-11-25 21:03:32 +0100

    remove comments

--- SplitExplosion.gd
+++ SplitExplosion.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
-func _ready():
-    print("ready")
-    print("position")
-
 func _on_Timer_timeout():
     $Particles2D.emitting = false
     $FreeTimer.start()
 
 func init(_position):
-    print("got position")
-    print(_position)
     position = _position
 
 func _on_FreeTimer_timeout():
