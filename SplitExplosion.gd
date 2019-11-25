80330c737ddfd4413158d8852019882141cd0c55
Johnnie Hård <johnnie@synvinkel.org>
2019-11-25 20:40:28 +0100

    queue_free the explosion when done

--- SplitExplosion.gd
+++ SplitExplosion.gd
@@ -6,8 +6,12 @@ func _ready():
 
 func _on_Timer_timeout():
     $Particles2D.emitting = false
+    $FreeTimer.start()
 
 func init(_position):
     print("got position")
     print(_position)
     position = _position
+
+func _on_FreeTimer_timeout():
+    queue_free()
